757c8b4d1f1acb4538724ba299f67cfd66cda406
Fillipe <fillipe484@gmail.com>
2021-10-15 17:43:29 -0300

    feat: dealing with exceptions

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,47 +1,7 @@
-// import { NextFunction, Request, Response } from "express";
-// import { verify } from "jsonwebtoken";
-
-// import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
-
-// interface IPayload {
-//   sub: string;
-// }
-
-// export async function ensureAuthenticated(
-//   request: Request,
-//   response: Response,
-//   next: NextFunction
-// ) {
-//   const authHeader = request.headers.authorization;
-
-//   if (!authHeader) {
-//     throw new Error("Token missing");
-//   }
-
-//   const [, token] = authHeader.split(" ");
-
-//   try {
-//     const { sub: user_id } = verify(
-//       token,
-//       "698dc19d489c4e4db73e28a713eab07b"
-//     ) as IPayload;
-
-//     const usersRepository = new UsersRepository();
-//     const user = await usersRepository.findById(user_id);
-
-//     if (!user) {
-//       throw new Error("User does not exists!");
-//     }
-
-//     next();
-//   } catch {
-//     throw new Error("Invalid Token");
-//   }
-// }
-
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
+import { AppError } from "../errors/AppError";
 import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
 
 interface IPayload {
@@ -56,7 +16,7 @@ export async function ensureAuthenticated(
   const authHeader = request.headers.authorization;
 
   if (!authHeader) {
-    throw new Error("Token missing!");
+    throw new AppError("Token missing!", 401);
   }
 
   const [, token] = authHeader.split(" ");
@@ -72,11 +32,11 @@ export async function ensureAuthenticated(
     const user = usersRepository.findById(user_id);
 
     if (!user) {
-      throw new Error("User does not exits!");
+      throw new AppError("User does not exits!", 401);
     }
 
     next();
   } catch (error) {
-    throw new Error("Invalid token");
+    throw new AppError("Invalid token", 401);
   }
 }
--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -3,6 +3,7 @@ import { compare } from "bcryptjs";
 import { sign } from "jsonwebtoken";
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "../../../../errors/AppError";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
@@ -29,13 +30,13 @@ class AuthenticateUserUseCase {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
-      throw new Error("Email or Password Incorret");
+      throw new AppError("Email or Password Incorret");
     }
 
     const passwordMatch = await compare(password, user.password);
 
     if (!passwordMatch) {
-      throw new Error("Email or Password Incorret");
+      throw new AppError("Email or Password Incorret");
     }
 
     const token = sign({}, "698dc19d489c4e4db73e28a713eab07b", {
