07b32c03006efead2548484c1c71db386f4a3076
Fillipe <fillipe484@gmail.com>
2021-10-16 17:50:31 -0300

    feat: tests create category

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,17 +1,52 @@
+import { AppError } from "../../../../errors/AppError";
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+let createCategoryUseCase: CreateCategoryUseCase;
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+
 describe("Create Category", () => {
-  it("Wait that 2 + 2 equals 4", () => {
-    const soma = 2 + 2;
-
-    const result = 4;
-
-    expect(soma).toBe(result);
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(
+      categoriesRepositoryInMemory
+    );
   });
 
-  it("Wait that 2 + 2 differs from 5", () => {
-    const soma = 2 + 2;
+  it("Should be able to create a new category", async () => {
+    const category = {
+      name: "Category test",
+      description: "Category description test",
+    };
 
-    const result = 5;
+    await createCategoryUseCase.execute({
+      name: category.name,
+      description: category.description,
+    });
 
-    expect(soma).not.toBe(result);
+    const categoryCreated = await categoriesRepositoryInMemory.findByName(
+      category.name
+    );
+
+    expect(categoryCreated).toHaveProperty("id");
+  });
+
+  it("Should not be able to create a new category with name already exists", async () => {
+    expect(async () => {
+      const category = {
+        name: "Category test",
+        description: "Category description test",
+      };
+
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description,
+      });
+
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description,
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
