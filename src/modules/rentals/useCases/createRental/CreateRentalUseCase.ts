1250d7f8621e59252bf18625e1c29255216c8222
Fillipe <fillipe484@gmail.com>
2021-10-26 19:59:41 -0300

    feat: dealing with time using dayjs

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,6 +1,11 @@
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+
 import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
 import { AppError } from "@shared/errors/AppError";
+
+dayjs.extend(utc);
 
 interface IRequest {
   user_id: string;
@@ -20,6 +25,8 @@ class CreateRentalUseCase {
       car_id
     );
 
+    const minimumHour = 24;
+
     if (carUnavailable) {
       throw new AppError("Car is unavailable");
     }
@@ -30,6 +37,18 @@ class CreateRentalUseCase {
 
     if (rentalOpenToUser) {
       throw new AppError("There is a rental in progress for this user");
+    }
+
+    const expectedReturnDateFormat = dayjs(expected_return_date)
+      .utc()
+      .local()
+      .format();
+
+    const dateNow = dayjs().utc().local().format();
+    const compare = dayjs(expectedReturnDateFormat).diff(dateNow, "hours");
+
+    if (compare < minimumHour) {
+      throw new AppError("Invalid return time!");
     }
 
     const rental = await this.rentalsRepository.create({
