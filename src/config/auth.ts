e69445603bf7cee393311e060020faba93141314
Fillipe <fillipe484@gmail.com>
2021-11-02 13:19:48 -0300

    chore: update refresh token expire time

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   secret_token: "698dc19d489c4e4db73e28a713eab07b",
-  expires_in_token: "30s",
+  expires_in_token: "15m",
   secret_refresh_token: "0ff201efc5bcffacaf572abcf01e5a7a",
   expires_in_refresh_token: "30d",
   expires_refresh_token_days: 30,
